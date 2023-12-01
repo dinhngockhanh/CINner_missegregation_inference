1aede51a1ef5024a0f179fe7a9315e62382a7424
Zijin <zx2406@columbia.edu>
2023-12-01 14:41:00 -0500

    updates

--- vignettes/program_medicc_testing.r
+++ vignettes/program_medicc_testing.r
@@ -39,109 +39,109 @@ sapply(files_sources, source)
 setwd(R_workplace)
 # devtools::install_github("dinhngockhanh/CancerSimulator", force = TRUE)
 # # ==================================================IMPORTANT PARAMETERS
-# #   Number of simulations
+# # #   Number of simulations
 n_simulations <- 1200
-#   Number of cells sampled in each simulation
-n_cells <- 500
-#   Bounds for ground-truth selection rates (1/r -> r)
-bound_ground_truth_arm_s <- 1.15
-#   Statistics for the study
-list_targets_library <- c(
-    #---Single-cell DNA: clonal/subclonal CNAs
-    "data=sc;target=genome;statistic=mean;variable=event_count;type=clonal;event=missegregation",
-    "data=sc;target=genome;statistic=mean;variable=event_count;type=subclonal;event=missegregation",
-    #---Single-cell DNA: phylo stats for tips
-    "data=sc;target=genome;statistic=mean;variable=cherries", # number of internal nodes with 2 tips
-    "data=sc;target=genome;statistic=mean;variable=pitchforks", # number of internal tips with 3 tips
-    "data=sc;target=genome;statistic=mean;variable=IL_number", # number of internal nodes with single tip childs
-    "data=sc;target=genome;statistic=mean;variable=avgLadder", # mean size of ladder (sequence of internal nodes, each with single tip childs)
-    #---Single-cell DNA: phylo stats for balance
-    "data=sc;target=genome;statistic=mean;variable=stairs", # proportion of subtrees that are imbalanced
-    "data=sc;target=genome;statistic=mean;variable=colless", # balance index of phylogeny tree
-    "data=sc;target=genome;statistic=mean;variable=sackin", # balance index of phylogeny tree
-    "data=sc;target=genome;statistic=mean;variable=B2", # balance index of phylogeny tree
-    "data=sc;target=genome;statistic=mean;variable=maxDepth" # height of phylogeny tree
-)
-# # ===============================================GROUND TRUTH PARAMETERS
-cell_lifespan <- 30
-T_0 <- list(0, "year")
-T_end <- list(80, "year")
-Table_sample <- data.frame(Sample_ID = c("SA01"), Cell_count = c(1000), Age_sample = c(80))
-selection_model <- "chrom-arm-selection"
-#---Probabilities of CNA
-prob_CN_missegregation <- 2e-4
+# #   Number of cells sampled in each simulation
+# n_cells <- 500
+# #   Bounds for ground-truth selection rates (1/r -> r)
+# bound_ground_truth_arm_s <- 1.15
+# #   Statistics for the study
+# list_targets_library <- c(
+#     #---Single-cell DNA: clonal/subclonal CNAs
+#     "data=sc;target=genome;statistic=mean;variable=event_count;type=clonal;event=missegregation",
+#     "data=sc;target=genome;statistic=mean;variable=event_count;type=subclonal;event=missegregation",
+#     #---Single-cell DNA: phylo stats for tips
+#     "data=sc;target=genome;statistic=mean;variable=cherries", # number of internal nodes with 2 tips
+#     "data=sc;target=genome;statistic=mean;variable=pitchforks", # number of internal tips with 3 tips
+#     "data=sc;target=genome;statistic=mean;variable=IL_number", # number of internal nodes with single tip childs
+#     "data=sc;target=genome;statistic=mean;variable=avgLadder", # mean size of ladder (sequence of internal nodes, each with single tip childs)
+#     #---Single-cell DNA: phylo stats for balance
+#     "data=sc;target=genome;statistic=mean;variable=stairs", # proportion of subtrees that are imbalanced
+#     "data=sc;target=genome;statistic=mean;variable=colless", # balance index of phylogeny tree
+#     "data=sc;target=genome;statistic=mean;variable=sackin", # balance index of phylogeny tree
+#     "data=sc;target=genome;statistic=mean;variable=B2", # balance index of phylogeny tree
+#     "data=sc;target=genome;statistic=mean;variable=maxDepth" # height of phylogeny tree
+# )
+# # # ===============================================GROUND TRUTH PARAMETERS
+# cell_lifespan <- 30
+# T_0 <- list(0, "year")
+# T_end <- list(80, "year")
+# Table_sample <- data.frame(Sample_ID = c("SA01"), Cell_count = c(1000), Age_sample = c(80))
+# selection_model <- "chrom-arm-selection"
+# #---Probabilities of CNA
+# prob_CN_missegregation <- 2e-4
 
-prob_neutral_CN_focal_amplification <- 1e-4
-prob_neutral_CN_focal_deletion <- 1e-4
+# prob_neutral_CN_focal_amplification <- 1e-4
+# prob_neutral_CN_focal_deletion <- 1e-4
 
-#---Viability thresholds
-bound_maximum_CN <- 8
-bound_average_ploidy <- 4.5
-bound_homozygosity <- 10
-#---Population dynamics
-vec_time <- T_0[[1]]:T_end[[1]]
-L <- 10000
-t_0 <- 20
-k <- 0.3
-vec_cell_count <- L / (1 + exp(-k * (vec_time - t_0)))
-table_population_dynamics <- cbind(vec_time, vec_cell_count)
-#---Initialize model variables
-model_variables <- BUILD_general_variables(
-    cell_lifespan = cell_lifespan,
-    T_0 = T_0, T_end = T_end,
-    CN_arm_level = TRUE,
-    Table_sample = Table_sample,
-    prob_CN_missegregation = prob_CN_missegregation,
-    prob_neutral_CN_focal_amplification = prob_neutral_CN_focal_amplification,
-    prob_neutral_CN_focal_deletion = prob_neutral_CN_focal_deletion,
-    selection_model = selection_model,
-    bound_average_ploidy = bound_average_ploidy,
-    bound_homozygosity = bound_homozygosity,
-    table_population_dynamics = table_population_dynamics
-)
-#---Set up (randomized) chromosome arm selection rates
-arm_id <- c(
-    paste0(model_variables$cn_info$Chromosome, "p"),
-    paste0(model_variables$cn_info$Chromosome, "q")
-)
-arm_chromosome <- rep(model_variables$cn_info$Chromosome, 2)
-arm_start <- c(
-    rep(1, length(model_variables$cn_info$Chromosome)),
-    model_variables$cn_info$Centromere_location + 1
-)
-arm_end <- c(
-    model_variables$cn_info$Centromere_location,
-    model_variables$cn_info$Bin_count
-)
-arm_s <- rep(1, length(arm_id))
-set.seed(1)
-for (i in 1:length(arm_s)) {
-    if (grepl("q$", arm_id[i])) {
-        arm_s[i] <- 1
-    } else if (grepl("p$", arm_id[i])) {
-        arm_s[i] <- runif(1, 1, bound_ground_truth_arm_s)
-        if (runif(1) < 0.5) arm_s[i] <- 1 / arm_s[i]
-    }
-}
-set.seed(NULL)
-table_arm_selection_rates <- data.frame(Arm_ID = arm_id, Chromosome = arm_chromosome, Bin_start = arm_start, Bin_end = arm_end, s_rate = arm_s)
-model_variables <- BUILD_driver_library(model_variables = model_variables, table_arm_selection_rates = table_arm_selection_rates, )
-#---Set up initial cell population
-cell_count <- 20
-CN_matrix <- BUILD_cn_normal_autosomes(model_variables$cn_info)
-drivers <- list()
-model_variables <- BUILD_initial_population(model_variables = model_variables, cell_count = cell_count, CN_matrix = CN_matrix, drivers = drivers)
-#---Save model variables
-model_name <- "Medicc_testing"
-model_variables <- CHECK_model_variables(model_variables)
-folder_workplace <- paste0(model_name, "/")
+# #---Viability thresholds
+# bound_maximum_CN <- 8
+# bound_average_ploidy <- 4.5
+# bound_homozygosity <- 10
+# #---Population dynamics
+# vec_time <- T_0[[1]]:T_end[[1]]
+# L <- 10000
+# t_0 <- 20
+# k <- 0.3
+# vec_cell_count <- L / (1 + exp(-k * (vec_time - t_0)))
+# table_population_dynamics <- cbind(vec_time, vec_cell_count)
+# #---Initialize model variables
+# model_variables <- BUILD_general_variables(
+#     cell_lifespan = cell_lifespan,
+#     T_0 = T_0, T_end = T_end,
+#     CN_arm_level = TRUE,
+#     Table_sample = Table_sample,
+#     prob_CN_missegregation = prob_CN_missegregation,
+#     prob_neutral_CN_focal_amplification = prob_neutral_CN_focal_amplification,
+#     prob_neutral_CN_focal_deletion = prob_neutral_CN_focal_deletion,
+#     selection_model = selection_model,
+#     bound_average_ploidy = bound_average_ploidy,
+#     bound_homozygosity = bound_homozygosity,
+#     table_population_dynamics = table_population_dynamics
+# )
+# #---Set up (randomized) chromosome arm selection rates
+# arm_id <- c(
+#     paste0(model_variables$cn_info$Chromosome, "p"),
+#     paste0(model_variables$cn_info$Chromosome, "q")
+# )
+# arm_chromosome <- rep(model_variables$cn_info$Chromosome, 2)
+# arm_start <- c(
+#     rep(1, length(model_variables$cn_info$Chromosome)),
+#     model_variables$cn_info$Centromere_location + 1
+# )
+# arm_end <- c(
+#     model_variables$cn_info$Centromere_location,
+#     model_variables$cn_info$Bin_count
+# )
+# arm_s <- rep(1, length(arm_id))
+# set.seed(1)
+# for (i in 1:length(arm_s)) {
+#     if (grepl("q$", arm_id[i])) {
+#         arm_s[i] <- 1
+#     } else if (grepl("p$", arm_id[i])) {
+#         arm_s[i] <- runif(1, 1, bound_ground_truth_arm_s)
+#         if (runif(1) < 0.5) arm_s[i] <- 1 / arm_s[i]
+#     }
+# }
+# set.seed(NULL)
+# table_arm_selection_rates <- data.frame(Arm_ID = arm_id, Chromosome = arm_chromosome, Bin_start = arm_start, Bin_end = arm_end, s_rate = arm_s)
+# model_variables <- BUILD_driver_library(model_variables = model_variables, table_arm_selection_rates = table_arm_selection_rates, )
+# #---Set up initial cell population
+# cell_count <- 20
+# CN_matrix <- BUILD_cn_normal_autosomes(model_variables$cn_info)
+# drivers <- list()
+# model_variables <- BUILD_initial_population(model_variables = model_variables, cell_count = cell_count, CN_matrix = CN_matrix, drivers = drivers)
+# #---Save model variables
+# model_name <- "Medicc_testing"
+# model_variables <- CHECK_model_variables(model_variables)
+# folder_workplace <- paste0(model_name, "/")
 # ==============GET TABLE OF CHROMOSOME LENGTHS AND CENTROMERE LOCATIONS
-cn_table <- model_variables$cn_info
-cn_bin_length <- as.numeric(model_variables$general_variables$Value[which(model_variables$general_variables$Variable == "size_CN_block_DNA")])
-cn_table$Length <- cn_table$Bin_count * cn_bin_length
-cn_table$Centromere <- cn_table$Centromere_location * cn_bin_length
-vec_CN_block_no <<- model_variables$cn_info$Bin_count
-vec_centromeres <<- model_variables$cn_info$Centromere_location
+# cn_table <- model_variables$cn_info
+# cn_bin_length <- as.numeric(model_variables$general_variables$Value[which(model_variables$general_variables$Variable == "size_CN_block_DNA")])
+# cn_table$Length <- cn_table$Bin_count * cn_bin_length
+# cn_table$Centromere <- cn_table$Centromere_location * cn_bin_length
+# vec_CN_block_no <<- model_variables$cn_info$Bin_count
+# vec_centromeres <<- model_variables$cn_info$Centromere_location
 # ======================================================MAKE SIMULATIONS
 # simulator_full_program(
 #     model = model_variables, model_prefix = model_name,
@@ -188,46 +188,48 @@ vec_centromeres <<- model_variables$cn_info$Centromere_location
 # )
 # =======================================TRANSFERING CSV TO MEDICC INPUTS
 
-# cat(paste0("Transferring ", n_simulations, " csvs to input format of medicc2...\n"))
-# n_cores <- max(detectCores() - 1, 1)
-# cl <- makePSOCKcluster(n_cores)
-# clusterExport(cl, varlist = c("read.csv", "write.table", "R_workplace", "as.integer", "grep"))
-# e <- new.env()
-# e$libs <- .libPaths()
-# clusterExport(cl, "libs", envir = e)
-# clusterEvalQ(cl, .libPaths(libs))
+cat(paste0("Transferring ", n_simulations, " csvs to input format of medicc2...\n"))
+n_cores <- max(detectCores() - 1, 1)
+cl <- makePSOCKcluster(n_cores)
+clusterExport(cl, varlist = c("read.csv", "write.table", "R_workplace", "as.integer", "grep"))
+e <- new.env()
+e$libs <- .libPaths()
+clusterExport(cl, "libs", envir = e)
+clusterEvalQ(cl, .libPaths(libs))
 
-# pbo <- pboptions(type = "txt")
-# mediccinput <- pblapply(cl = cl, X = 1:n_simulations, FUN = function(i) {
-#     R_inputplace <- paste0(R_workplace, "/Medicc_testing_", i)
-#     data <- read.csv(paste0(R_inputplace, "/Medicc_testing_cn_profiles_long_1", ".csv"))
-#     new_df <- data.frame(matrix(nrow = nrow(data), ncol = 6))
-#     colnames(new_df) <- c("sample_id", "chrom", "start", "end", "cn_a", "cn_b")
-#     new_df$sample_id <- data$cell_id
-#     new_df$chrom <- paste0("chr", data$chr)
-#     new_df$start <- as.integer(data$start)
-#     new_df$end <- as.integer(data$end)
-#     new_df$cn_a <- data$Maj
-#     new_df$cn_b <- data$Min
-#     file_path <- paste0(R_workplace, "/Simulation", i, ".tsv")
-#     write.table(new_df, file = file_path, sep = "\t", quote = FALSE, row.names = FALSE)
-# })
-# stopCluster(cl)
-
-focal_rate_df <- data.frame(matrix(nrow = n_simulations, ncol = 24))
-R_inputplace <- paste0(R_workplace, "/Medicc_testing_", 1)
-ground_truth_params <- read.csv(paste0(R_inputplace, "/Medicc_testing_ground_truth_1.csv"))
-colnames(focal_rate_df) <- c("Simulation_id", ground_truth_params$Parameter)
-for (i in 1:n_simulations) {
-    focal_rate_df$Simulation_id[i] <- paste0("Simulation_", i)
+pbo <- pboptions(type = "txt")
+mediccinput <- pblapply(cl = cl, X = 1:n_simulations, FUN = function(i) {
     R_inputplace <- paste0(R_workplace, "/Medicc_testing_", i)
-    ground_truth_params <- read.csv(paste0(R_inputplace, "/Medicc_testing_ground_truth_1.csv"))
-    for (para in ground_truth_params$Parameter){
-        focal_rate_df[[para]][i] <- ground_truth_params$Value[which(ground_truth_params$Parameter == para)]
-    }
+    data <- read.csv(paste0(R_inputplace, "/Medicc_testing_cn_profiles_long_1", ".csv"))
+    new_df <- data.frame(matrix(nrow = nrow(data), ncol = 6))
+    colnames(new_df) <- c("sample_id", "chrom", "start", "end", "cn_a", "cn_b")
+    new_df$sample_id <- data$cell_id
+    new_df$chrom <- paste0("chr", data$chr)
+    new_df$start <- as.integer(data$start)
+    new_df$end <- as.integer(data$end)
+    new_df$cn_a <- data$Maj
+    new_df$cn_b <- data$Min
+    # print(grep("library", new_df$sample_id))
+    new_df <- new_df[grep("Library", new_df$sample_id),]
+    file_path <- paste0(R_workplace, "/Simulation", i, ".tsv")
+    write.table(new_df, file = file_path, sep = "\t", quote = FALSE, row.names = FALSE)
+})
+stopCluster(cl)
+
+# focal_rate_df <- data.frame(matrix(nrow = n_simulations, ncol = 24))
+# R_inputplace <- paste0(R_workplace, "/Medicc_testing_", 1)
+# ground_truth_params <- read.csv(paste0(R_inputplace, "/Medicc_testing_ground_truth_1.csv"))
+# colnames(focal_rate_df) <- c("Simulation_id", ground_truth_params$Parameter)
+# for (i in 1:n_simulations) {
+#     focal_rate_df$Simulation_id[i] <- paste0("Simulation_", i)
+#     R_inputplace <- paste0(R_workplace, "/Medicc_testing_", i)
+#     ground_truth_params <- read.csv(paste0(R_inputplace, "/Medicc_testing_ground_truth_1.csv"))
+#     for (para in ground_truth_params$Parameter){
+#         focal_rate_df[[para]][i] <- ground_truth_params$Value[which(ground_truth_params$Parameter == para)]
+#     }
     
-}
-write.csv(focal_rate_df, "params_df.csv")
+# }
+# write.csv(focal_rate_df, "params_df.csv")
 # =======================================COMPUTE GROUND-TRUTH STATISTICS
 #---Get single-cell statistics & CN profiles
 #   Get statistics & clonal CN profiles for each single-cell sample
